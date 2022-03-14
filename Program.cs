e5946e44481a5128840ed7ada295ca9c2fa5f5bd
JOY <joybosu@gmail.com>
2022-03-14 20:39:03 +0530

    Welcome to my AddressBook Program using Lambda

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using AddressBookLamdaDemo;
+//using LamdaDemo;
+using System;
+using System.Collections.Generic;
+
+namespace AddressBook
+{
+    class Program
+    {
+        static void Main(string[] args)
+
+        {
+            Console.WriteLine("Welcome to My AddressBook Problem Using Lambda");
+
+            
+        }
+        
+    }
+   
+}
